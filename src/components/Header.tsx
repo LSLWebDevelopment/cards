a064e5cda63731d23621211aa8ee4aa9cda09a09
LSL Web Development <lslwebdevelopment@gmail.com>
2025-08-31 16:14:55 -0300

    Updated card styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ interface HeaderProps {
 
 export function Header({ header }: HeaderProps) {
   return (
-    <header className="mt-10 border-5 border-emerald-500 p-6 bg-emerald-200">
+    <header className="mt-10 border-3 text-center border-emerald-300 p-6 bg-emerald-100">
       <h1 className="text-4xl font-semibold text-gray-700">{header}</h1>
     </header>
   );
